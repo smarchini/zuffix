d697e30df93172eda6258401342d1bbf19e3510d
Stefano Marchini <pacman616@gmail.com>
2021-11-14 00:23:07 +0100

    Rabin-Karp uses the modular multiplicative inverse

--- zuffix/hash/RabinKarpHash.hpp
+++ zuffix/hash/RabinKarpHash.hpp
@@ -7,24 +7,24 @@ namespace zarr {
 
 template <typename T> class RabinKarpHash {
   public:
-	const uint64_t m = 0x3; // WARNING must be small, otherwise it ruins the division
+	// md = 1 mod 2^64
+	const uint64_t m = 0xf7c35;
+	const uint64_t d = 0xe94870e35dd4f61d;
 
   private:
 	T *string;
 	uint64_t state = 0;
 	size_t l = 0, r = 0;
-	uint64_t mpow = 1;
+	uint64_t pow = 1;
 
   public:
 	RabinKarpHash(T *string) : string(string) {}
 
 	uint64_t operator()(size_t from, size_t length) {
-		// WARNING division is really bad due to multiplication mod 2^64
-		for (; l > from; l--, mpow *= m) state = state + string[l - 1] * mpow;
-		for (; r < from + length; r++, mpow *= m) state = state * m + string[r];
-		// NOTE mpow/=m must b after mpow*=m, otherwise mpow may become zero
-		for (; l < from; l++, mpow /= m) state = state - string[l] * mpow / m;
-		for (; r > from + length; r--, mpow /= m) state = (state - string[r - 1]) / m;
+		for (; l < from; l++, pow *= d) state -= string[l] * pow * d;
+		for (; l > from; l--, pow *= m) state += string[l - 1] * pow;
+		for (; r < from + length; r++, pow *= m) state = state * m + string[r];
+		for (; r > from + length; r--, pow *= d) state = (state - string[r - 1]) * d;
 		return state;
 	}
 };
