cefe50fc34c9ef12fb44b2e2252bc619be1ca288
Stefano Marchini <pacman616@gmail.com>
2023-03-10 14:20:47 +0100

    Tweaking

--- benchmark/findrandom.cpp
+++ benchmark/findrandom.cpp
@@ -1,51 +1,57 @@
-#include <benchmark/benchmark.h>
-
 #include "common.hpp"
-
-// 50MB
-#define N (1ULL << 25)
-#define COMMA ,
-#define ALPHABET 4
+#include <benchmark/benchmark.h>
 
 using namespace zarr;
 using namespace sux::util;
 
-constexpr uint8_t charset[] = {0,   1,   2,   3,   4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,
-							   32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,
-							   64,  65,  66,  67,  68,  69,  70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,
-							   96,  97,  98,  99,  100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127,
-							   128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
-							   160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191,
-							   192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223,
-							   224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254};
+String<SIGMA_T> *string;
+constexpr size_t N = 1ULL << 28;
+xoroshiro128plus_engine rng(0x2023);
+
+STRUCTURE_T *ds;
+EnhancedSuffixArray<SIGMA_T> *reference;
 
 static void args(benchmark::internal::Benchmark *b) {
-	for (int i = 1; i <= 24; i++) b->Args({1L << i});
+    for (int i = 1; i <= 24; i++) b->Args({1L << i});
 }
 
 static void BM_run(benchmark::State &state) {
-    static std::random_device rd;
-    static zarr::xoroshiro128plus_engine rng(1337);
-    static zarr::String<uint8_t> t = random(N, charset, 2);
-    t[t.length() - 1] = t.DOLLAR; // TODO vedere se serve, sistemare common.hpp
-    static zarr::EnhancedSuffixArray<uint8_t> reference(t.substring(0, N));
-    static STRUCTURE_T ds(t.substring(0, N));
     size_t m = state.range(0);
-    std::uniform_int_distribution<uint64_t> dist(0, N - m - 1);
-    int64_t empty = 0, errors = 0;
+    std::uniform_int_distribution<uint64_t> dist(0, N - m);
+    int64_t empty = 0, errors = 0, occurrences = 0;
     for (auto _ : state) {
         state.PauseTiming();
         size_t from = dist(rng);
-        auto p = t.substring(from, m);
-        auto expected = reference.find(p);
+        auto p = string->substring(from, m);
+        auto expected = reference->find(p);
         empty += expected.isEmpty();
+        occurrences += expected.length();
         benchmark::DoNotOptimize(p);
         state.ResumeTiming();
-        benchmark::DoNotOptimize(errors += ds.find(p) != expected);
+        benchmark::DoNotOptimize(errors += ds->find(p) != expected);
     }
     state.counters["empty"] = empty;
     state.counters["errors"] = errors;
+    state.counters["occurrences"] = occurrences;
 }
 BENCHMARK(BM_run)->Apply(args)->Iterations(10000);
 
-BENCHMARK_MAIN();
+int main(int argc, char **argv) {
+    string = new String<SIGMA_T>(random(N, (const SIGMA_T*)"ACGT", 4));
+
+    ds = new STRUCTURE_T(string->substring(0, N));
+    reference = new EnhancedSuffixArray(string->substring(0, N));
+
+    // Google Benchmark's stuff
+    char arg0_default[] = "benchmark";
+    char *args_default = arg0_default;
+    if (!argv) {
+        argc = 1;
+        argv = &args_default;
+    }
+    ::benchmark::Initialize(&argc, argv);
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
