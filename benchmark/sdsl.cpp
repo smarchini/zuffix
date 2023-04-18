1b55c05601b628c6e2654c0777e23815a71eeb6f
Stefano Marchini <pacman616@gmail.com>
2023-04-18 17:00:38 +0200

    Tweaking

---
+++ benchmark/sdsl.cpp
@@ -0,0 +1,74 @@
+#include "common.hpp"
+#include <benchmark/benchmark.h>
+
+using namespace zarr;
+using namespace sux::util;
+
+std::mt19937 rng(2023);
+std::span<SIGMA_T> text;
+static constexpr size_t textsize = 200ULL << 20; // TODO: trovare un modo di prenderlo da text (all'interno di args)
+
+static void args(benchmark::internal::Benchmark *b) {
+    for (size_t k = 1; k * 10 < textsize; k *= 10)
+        for (size_t i = k; i < k * 10; i += k) b->Arg(i);
+}
+
+static void BM_run(benchmark::State &state) {
+    static bool is_built = false;
+    static auto begin = std::chrono::high_resolution_clock::now();
+    static SDSL_STRUCTURE_T ds(text);
+    static auto end = std::chrono::high_resolution_clock::now();
+    static auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+    if (!is_built) {
+        std::cout << "Data structureconstruction time: " << time << " ns" << std::endl;
+        text.data()[text.size() - 1] = std::numeric_limits<SIGMA_T>::max();
+        is_built = true;
+    }
+    static EnhancedSuffixArray<SIGMA_T> reference(text);
+    size_t m = state.range(0);
+    std::uniform_int_distribution<uint64_t> dist(0, text.size() - m - 1);
+    int64_t empty = 0, errors = 0, occurrences = 0;
+    for (auto _ : state) {
+        state.PauseTiming();
+        size_t from = dist(rng);
+        auto p = text.subspan(from, m);
+        auto expected = reference.find(p).length();
+        empty += expected == 0;
+        occurrences += expected;
+        benchmark::DoNotOptimize(p);
+        state.ResumeTiming();
+        benchmark::DoNotOptimize(errors += ds.SDSL_COUNT_OP(p) != expected);
+    }
+    state.counters["empty"] = empty;
+    state.counters["errors"] = errors;
+    state.counters["occurrences"] = occurrences;
+    state.counters["length"] = m;
+}
+BENCHMARK(BM_run)->Apply(args)->Iterations(10000);
+
+int main(int argc, char **argv) {
+    std::ifstream file(argv[1], std::ios::in);
+    file.seekg(0, file.end);
+    size_t filesize = file.tellg();
+    file.seekg(0, file.beg);
+    assert(filesize % sizeof(SIGMA_T) == 0 && "Bad file size.");
+
+    size_t length = filesize / sizeof(SIGMA_T) + 1;
+    sux::util::Vector<SIGMA_T> buffer(length);
+    file.read((char *)&buffer, filesize);
+    buffer[length - 1] = 0; // std::numeric_limits<SIGMA_T>::max();
+    text = std::span<SIGMA_T>(&buffer, length); // global variable
+
+    // Google Benchmark's stuff
+    char arg0_default[] = "benchmark";
+    char *args_default = arg0_default;
+    if (!argv) {
+        argc = 1;
+        argv = &args_default;
+    }
+    ::benchmark::Initialize(&argc, argv);
+    // if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
